455ae4e2a777926edd450a82a822906015a89107
haythem-dj <haythemdjebbar5@gmail.com>
2026-03-17 18:36:59 +0100

    committing

--- simulation.py
+++ simulation.py
@@ -3,6 +3,14 @@ from scipy.integrate import solve_ivp
 import matplotlib.pyplot as plt
 
 
+def rpm2rad(s):
+    return s * (np.pi / 30)
+
+
+def rad2rpm(s):
+    return s * (30 / np.pi)
+
+
 def model(t, X, U, params):
     R = params["R"]
     L = params["L"]
@@ -10,7 +18,6 @@ def model(t, X, U, params):
     J = params["J"]
     Kv = params["Kv"]
     Kt = params["Kt"]
-    Tl = params["Tl"]
 
     V = U[0]
 
@@ -18,7 +25,7 @@ def model(t, X, U, params):
     w = X[1]
 
     di = (1 / L) * V - (R / L) * i - (Kv / L) * w
-    dw = (Kt / J) * i - (b / J) * w - Tl
+    dw = (Kt / J) * i - (b / J) * w
 
     return [di, dw]
 
@@ -28,13 +35,12 @@ params = {
     "L": 0.514e-3,
     "b": 1.11e-4,
     "J": 21.2e-7,
-    "Kv": 223 * (2 * np.pi / 60),
+    "Kv": 1 / rpm2rad(223),
     "Kt": 42.9e-3,
-    "Tl": 20.0e-3,
 }
 
 tstart = 0
-tfinish = 1
+tfinish = 0.5
 tspan = np.linspace(tstart, tfinish, 500)
 X0 = [0, 0]
 U = [24]
@@ -44,44 +50,37 @@ sol = solve_ivp(model, (tstart, tfinish), X0, args=(U, params))
 
 t = np.array(sol.t)
 i = np.array(sol.y[0])
-w = np.array(sol.y[1])
+w = rad2rpm(np.array(sol.y[1]))
 V = np.full(len(i), U[0])
 T = params["Kt"] * i * 1e3
-Pt = V * i
-Pm = T * w
+Pe = V * i
+Pm = T * rpm2rad(w) * 1e-3
 
-w = w * (60 / (2 * np.pi))
+sols = [
+    {"name": "Voltage", "unit": "V", "title": "Input Voltage", "data": V},
+    {"name": "Current", "unit": "A", "title": "Motor Current", "data": i},
+    {"name": "Speed", "unit": "rpm", "title": "Motor Speed", "data": w},
+    {"name": "Torque", "unit": "mNm", "title": "Motor Torue", "data": T},
+    {"name": "Power", "unit": "W", "title": "Elecrical Power", "data": Pe},
+    {"name": "Power", "unit": "W", "title": "Mechanical Power", "data": Pm},
+]
+
+print("Specs:")
+for sol in sols:
+    print(
+        f"\t{sol['title']}: {sol['data'][-1]} {sol['unit']}, max: {max(sol['data'])} {sol['unit']}"
+    )
 
 fig, axes = plt.subplots(2, 3, sharex=True)
 axes = axes.flatten()
 
-axes[0].plot(t, V)
-axes[0].set_ylabel("Voltage(V)")
-axes[0].set_title("Input Voltage")
-
-axes[1].plot(t, i)
-axes[1].set_ylabel("Current(A)")
-axes[1].set_title("Motor Current")
-
-axes[2].plot(t, w)
-axes[2].set_ylabel("Speed(rpm)")
-axes[2].set_title("Motor Speed")
-
-axes[3].plot(t, T)
-axes[3].set_ylabel("Torque(mNm)")
-axes[3].set_title("Motor Torque")
-
-axes[4].plot(t, Pt)
-axes[4].set_ylabel("Power(W)")
-axes[4].set_title("Input Power")
-
-axes[5].plot(t, Pm)
-axes[5].set_ylabel("Power(W)")
-axes[5].set_title("Mechanical Power")
-
-for ax in axes:
+for i in range(len(sols)):
+    plot = sols[i]
+    ax = axes[i]
+    ax.plot(t, plot["data"])
+    ax.set_ylabel(f"{plot["name"]}({plot["unit"]})")
+    ax.set_xlabel("Time(s)")
     ax.grid(True)
 
-plt.xlabel("Time(s)")
 plt.tight_layout()
 plt.show()
